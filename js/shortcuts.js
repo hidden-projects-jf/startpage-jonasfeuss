c0c1f4dff52697735cf761ae53a8549609eb1b1c
jonasfeuss <74841015+jonasfeuss@users.noreply.github.com>
2023-05-03 23:17:37 +0200

    upload

---
+++ js/shortcuts.js
@@ -0,0 +1,74 @@
+export default [
+  {
+    category: "Web",
+    color: "cyan",
+    items: {
+      GitHub: "https://github.com",
+      Gatsby: "https://gatsbyjs.com/",
+      Netlify: "https://netlify.com/",
+      Cloudflare: "https://cloudflare.com/",
+      netcup: "https://ccp.netcup.de/",
+      ImprovMX: "https://app.improvmx.com",
+    },
+  },
+  {
+    category: "Work",
+    color: "red",
+    items: {
+      IServ: "https://has-hagen.de/",
+      Vertretungsplan: "https://has-hagen.de/iserv/infodisplay/file/10/infodisplay/0/Vertretungsplan/SuS/Tag2/subst_001.htm",
+      OneNote: "https://onenote.com",
+      Klassennotizbuch: "https://onenote.com/classnotebook",
+      Teams: "https://teams.office.com/",
+      Office: "https://office.com/",
+      NotionAnki: "https://2anki.net/",
+      Notion: "https://feuss.notion.site/",
+      Ordnerruecken: "https://www.avery-zweckform.com/tipp/vorlagen-ordnerruecken",
+      ChatGPT: "https://chat.openai.com/",
+    },
+  },
+  {
+    category: "Kalenderplanung",
+    color: "green",
+    items: {
+      Slack: "https://jonasfeuss.slack.com",
+      Reclaim: "https://app.reclaim.ai/",
+    },
+  },
+  {
+    category: "Unterhaltung",
+    color: "purple",
+    items: {
+      YouTube: "https://www.youtube.com/?gl=DE&hl=de",
+      Twitch: "https://www.twitch.tv/directory/following",
+    },
+  },
+  {
+    category: "Finanzen",
+    color: "blue",
+    items: {
+      Volksbank: "https://www.volksbankeg.de/services_auth/auth-ui/?v=07f11cfe8b0ae022&client_id=fkp&redirect_uri=https:%2F%2Fwww.volksbankeg.de%2Fservices_cloud%2Fportal%2Fportal-oauth%2Flogin",
+      VimPay: "https://vimpay.eu/login/show",
+      ErgoDepot: "https://mein-ergo-depot.de/",
+      Finanzen100: "https://www.finanzen100.de/portfolio",
+      Parqet: "https://app.parqet.com/",
+      Aktienfinder: "https://aktienfinder.net/",
+      AktienRechner: "https://rechneronline.de/aktien/",
+      Tradingdesk: "https://trading.boerse-stuttgart.de/",
+      TradingView: "https://www.tradingview.com/",
+      justETF: "https://www.justetf.com/de/",
+      fondsweb: "https://www.fondsweb.com/de",
+    },
+  },
+  {
+    category: "Social Media",
+    color: "gray",
+    items: {
+      Facebook: "https://www.facebook.com/",
+      FacebookBusiness: "https://business.facebook.com/",
+      Instagram: "https://www.instagram.com/",
+      Twitter: "https://twitter.com/home",
+      Reddit: "https://www.reddit.com/",
+    },
+  },
+];
