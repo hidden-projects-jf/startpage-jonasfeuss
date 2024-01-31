efbcaf94c6d4ea845a22f6481c93312cf31ad821
Jonas Feuß <74841015+jonasfeuss@users.noreply.github.com>
2024-01-31 17:00:21 +0000

    WebUntis hinzugefügt

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -18,6 +18,7 @@ export default [
       IServ: "https://has-hagen.de/",
       Vertretungsplan:
         "https://has-hagen.de/iserv/infodisplay/file/10/infodisplay/0/Vertretungsplan/SuS/Tag2/subst_001.htm",
+      WebUntis: "https://kos.webuntis.com/WebUntis/?school=Hermann-Allmers-Schule",
       Stundenplan:
         "https://has-hagen.de/iserv/fs/file/local/Files/Stundenplan/stundenplan-montag-bis-samstag.pdf",
       OneNote: "https://onenote.com",
